071b89c20ef7a573b43226ad0d615c13d568c906
mramirez-wizeline <miguel.ricardo@wizeline.com>
2023-08-25 09:53:11 -0500

    trying with different resource

--- main.tf
+++ main.tf
@@ -9,13 +9,18 @@ module "s3_bucket_landing" {
   object_ownership         = "ObjectWriter"
 }
 
-resource "aws_s3_object" "input_path" {
+resource "aws_s3_bucket_object" "input_folder" {
   bucket = var.s3_bucket_landing.name
-  acl    = "private"
-  key    = "${var.s3_bucket_landing.input}"
-  source = "/${var.s3_bucket_landing.input}"
-  etag = filemd5("/${var.s3_bucket_landing.input}")
+  key    = "${var.s3_bucket_landing.input}/"
 }
+
+#resource "aws_s3_object" "input_path" {
+#  bucket = var.s3_bucket_landing.name
+#  acl    = "private"
+#  key    = "${var.s3_bucket_landing.input}"
+#  source = "/${var.s3_bucket_landing.input}"
+#  etag = filemd5("/${var.s3_bucket_landing.input}")
+#}
 
 #resource "aws_s3_object" "output_path" {
 #  bucket = module.s3_bucket_landing.s3_bucket_id

55619cf4c9f60ac5310d8935f8de214f22322e75
Ricardo Ramirez <miriramire@gmail.com>
2023-09-03 10:29:18 -0500

    New lambda Function

--- lambda-iam.tf
+++ lambda-iam.tf
@@ -9,3 +9,23 @@ resource "aws_iam_role" "lambda_role" {
 
   assume_role_policy = "${file("iam/lambda-assume-policy.json")}"
 }
+
+resource "aws_iam_policy" "lambda_s3_policy" {
+  name        = "lambda-s3-policy"
+  description = "Policy for Lambda function to access S3 bucket"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = "s3:GetObject"
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::${var.s3_bucket_landing.name}/${var.s3_bucket_landing.input}/*"
+      },
+      {
+        Action   = "s3:PutObject"
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::${var.s3_bucket_landing.name}/${var.s3_bucket_landing.output}/*" 
+      }
+    ]
+  })
+}
--- main.tf
+++ main.tf
@@ -20,19 +20,24 @@ resource "aws_s3_object" "output_folder" {
 }
 
 # My lambda function
-module "lambda" {
-  source           = "terraform-aws-modules/lambda/aws"
-
+resource "aws_lambda_function" "s3_transform_function" {
   function_name    = var.lambda.function_name
   description      = "Example AWS Lambda using python with S3 trigger"
   handler          = var.lambda.handler
   runtime          = var.lambda.runtime
   filename         = var.lambda.lambda_zip_location
-  # source_code_hash = filebase64sha256("${var.lambda.lambda_zip_location}")
 
-  event = {
-    type          = "s3"
-    s3_bucket_arn = module.s3_bucket_landing.s3_bucket_arn
-    s3_bucket_id  = "${var.s3_bucket_landing.name}/${var.s3_bucket_landing.input}"
+  role          = "${aws_iam_role.lambda_role.arn}"
+  # source_code_hash = filebase64sha256("${var.lambda.lambda_zip_location}")
+}
+
+resource "aws_s3_bucket_notification" "s3_event_trigger" {
+  bucket = var.s3_bucket_landing.name
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.s3_transform_function.arn
+    events             = ["s3:ObjectCreated:*"]
+    filter_prefix      = ""
+    filter_suffix      = ""
   }
 }
